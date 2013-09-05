665d9f5357908b2a69d419ae5800d14a083afd01
Jon Rish <jon.m.rish@gmail.com>
2013-09-04 21:03:45 -0400

    add admin/users index, show, edit & update
    add admin only links in layout

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,37 @@
 class Admin::UsersController < ApplicationController
 
 	def index
-		@users = Users.all
+		@users = User.all
 	end
 
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+			redirect_to admin_user_path(@user), notice: 'User Updated'
+		else
+			redirect_to edit_admin_user_path(@user), notice: 'User Not Updated'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		redirect_to admin_users_path, notice: 'User Deleted'
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:admin)
+		end
 end
---
+++ spec/features/admin_manages_users_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+feature 'admin manages users' do
+
+	let!(:highlight_1) { FactoryGirl.create(:highlight) }
+	let!(:highlight_2) { FactoryGirl.create(:highlight) }
+	let!(:highlight_3) { FactoryGirl.create(:highlight) }
+
+
+	scenario 'admin views list of users' do
+		sign_in_as(highlight_1.user)
+		visit admin_users_path
+		expect(page).to have_content(highlight_1.user.id)
+		expect(page).to have_content(highlight_2.user.email)
+		expect(page).to have_content(highlight_3.user.admin)
+		expect(page).to have_content(highlight_1.user.highlights.count)
+		expect(page).to have_content(highlight_2.user.created_at.strftime("%Y-%m-%d"))
+	end
+
+	scenario 'admin views user details' do
+		sign_in_as(highlight_1.user)
+		visit admin_users_path
+		click_on "View User #{highlight_2.user.id}'s Details"
+		expect(current_path).to eql(admin_user_path(highlight_2.user))
+		expect(page).to have_content(highlight_2.user.id)
+		expect(page).to have_content(highlight_2.user.email)
+		expect(page).to have_content(highlight_2.user.admin)
+		expect(page).to have_content(highlight_2.user.created_at.strftime("%Y-%m-%d"))
+		expect(page).to have_content(highlight_2.name)
+		expect(page).to have_content(highlight_2.highlight_type.name)
+		expect(page).to have_content(highlight_2.created_at.strftime("%Y-%m-%d"))
+		expect(page).to have_content(highlight_2.state)
+	end
+
+	scenario 'admin edits user details' do
+		sign_in_as(highlight_1.user)
+		visit admin_user_path(highlight_2.user)
+		click_on 'Edit User'
+		expect(current_path).to eql(edit_admin_user_path(highlight_2.user))
+		select 'No', from: 'User Admin?'
+		click_on 'Update User'
+		expect(current_path).to eql(admin_user_path(highlight_2.user))
+		expect(highlight_2.user.admin).to eql(false)
+	end
+
+end
