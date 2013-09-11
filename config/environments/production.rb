47bf50ef15ef650c9a17974efe21bf30df4b7e00
Jon Rish <jon.m.rish@gmail.com>
2013-09-11 09:28:38 -0400

    modify domain name in email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Highlights3::Application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'youmaketheplay-staging.heroku.com',
+  :domain         => 'jonrish.mailgun.org',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
