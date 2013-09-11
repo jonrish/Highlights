ee8f874e24cb6c72b9fa05ed58f676de50cc0884
Jon Rish <jon.m.rish@gmail.com>
2013-09-11 08:51:06 -0400

    modify highlights controller
    scope variables to current user
    user where instead of find
    remove before_action for correct user
    add bottom-margin to highlight_type list items

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,6 +1,6 @@
 class HighlightsController < ApplicationController
 	before_action :authenticate_user!, except: [:new, :create]
-	before_action :correct_highlight?, except: [:new, :create]
+	# before_action :correct_highlight?, except: [:new, :create]
 	before_action :edit_highlight?, only: [:edit, :update, :destroy]
 
 	def show
@@ -32,7 +32,7 @@ class HighlightsController < ApplicationController
 	end
 
 	def update
-		@highlight = Highlight.find(params[:id])
+		@highlight = current_user.highlights.where(id: params[:id]).first
 
 		if @highlight.update(highlight_params)
 			redirect_to new_charge_path, notice: 'Highlight Updated'
@@ -53,10 +53,10 @@ class HighlightsController < ApplicationController
 			params.require(:highlight).permit(:name, :highlight_type_id, :nickname, :hometown, :school, :previous_teams, :bats, :throwing_hand, :position, :age, :family_friends, :opponent, :scenario, :notes, user_attributes: [:email, :password, :password_confirmation])
 		end
 
-		def correct_highlight?
-			@highlight = Highlight.find(params[:id])
-			redirect_to root_path, notice: 'You are not authorized to view that page.' unless current_user.id == @highlight.user_id
-		end
+		# def correct_highlight?
+		# 	@highlight = Highlight.find(params[:id])
+		# 	redirect_to root_path, notice: 'You are not authorized to view that page.' unless current_user.id == @highlight.user_id
+		# end
 
 		def edit_highlight?
 			@highlight = Highlight.find(params[:id])
